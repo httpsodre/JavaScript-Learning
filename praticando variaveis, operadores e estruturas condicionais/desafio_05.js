e23df127deef5b43d8365662d6c2bbbb8ee66cd1
httpsodre <camposalansodre@gmail.com>
2025-03-19 17:11:47 -0300

    teste debito

---
+++ praticando variaveis, operadores e estruturas condicionais/desafio_05.js
@@ -0,0 +1,17 @@
+/* Valor pago de um produto */
+const valorProduto = 100;
+const formaDePagamento = 1;
+
+if (formaDePagamento === 1){
+    const descontoDebito = (valorProduto * 0.1)
+console.log(valorProduto - descontoDebito)
+} else if (formadepagamento === 2){
+    const descontoPixDinheiro = (valorProduto * 0.15)
+    console.log(valorProduto-descontoPixDinheiro)
+}else if (formaDePagamento === 3){
+    console.log(valorProduto)
+}else {
+    const juros = (valorProduto * 0.1)
+    console.log(valorProduto + juros)
+}
+  
