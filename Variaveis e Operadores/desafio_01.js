f19964511b48692d5a97cf568b0f03f12dfc2afa
httpsodre <camposalansodre@gmail.com>
2025-03-19 13:33:07 -0300

    alteração na formatação

--- Variaveis e Operadores/desafio_01.js
+++ Variaveis e Operadores/desafio_01.js
@@ -1,8 +1,10 @@
 const combutivel = 6.34;
 const gastoDeCombustivel = 14.2;
 const distancia = 500;
+let rs = "R$"
+const espaco = " "
 
 let gastoDistancia = distancia - gastoDeCombustivel
-let valorViagem = gastoDistancia * combutivel
+let valorViagem = rs + espaco + gastoDistancia * combutivel
 
 console.log(valorViagem)
