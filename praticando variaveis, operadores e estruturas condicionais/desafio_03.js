0327e983e6680c802f673a076cd54c6b8e6646a2
httpsodre <camposalansodre@gmail.com>
2025-03-19 15:38:48 -0300

    teste2

--- praticando variaveis, operadores e estruturas condicionais/desafio_03.js
+++ praticando variaveis, operadores e estruturas condicionais/desafio_03.js
@@ -10,6 +10,6 @@ if (media > 7) {
     console.log("Aprovado!")
 }else if (media <= 7 ) {
     console.log ("Recuperação!")
-} else (media < 5);{
+} else if (media < 5){
     console.log("Reprovado!")
 }
