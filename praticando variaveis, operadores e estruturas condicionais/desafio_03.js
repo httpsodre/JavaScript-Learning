b76648c7d68dc81246575770550fd898a7c0c49d
httpsodre <camposalansodre@gmail.com>
2025-03-19 15:49:46 -0300

    teste 1 repro

--- praticando variaveis, operadores e estruturas condicionais/desafio_03.js
+++ praticando variaveis, operadores e estruturas condicionais/desafio_03.js
@@ -5,11 +5,12 @@ const nota2 = 3;
 const nota3 = 6;
 
 const media =(nota1 + nota2 + nota3)/3;
+console.log(media)
 
-if (media > 7) {
-    console.log("Aprovado!")
-}else if (media <= 7 ) {
-    console.log ("Recuperação!")
-} else if (media < 5){
+if (media < 5) {
     console.log("Reprovado!")
+}else if (media >=5 && media <=7) {
+    console.log ("Recuperação!")
+} else  {
+    console.log("Aprovado!")
 }
