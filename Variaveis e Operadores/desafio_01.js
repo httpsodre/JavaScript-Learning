94e68d7461edad58fdc6ef8ed2c8253823ace5d4
httpsodre <camposalansodre@gmail.com>
2025-03-19 13:42:51 -0300

    alteração nos preços

--- Variaveis e Operadores/desafio_01.js
+++ Variaveis e Operadores/desafio_01.js
@@ -1,10 +1,10 @@
 const combutivel = 6.34;
-const kmPorLitro = 14.2;
+const kmPorLitro = 14;
 const distanciaEmKm = 500;
-const rs = "R$"
-const espaco = " "
+const rs = "R$";
+const espaco = " ";
 
-const gastoDistancia = distanciaEmKm / kmPorLitro
-const valorViagem = rs + espaco + gastoDistancia * combutivel
+const gastoDistancia = distanciaEmKm / kmPorLitro;
+const valorViagem = rs + espaco + gastoDistancia * combutivel;
 
-console.log(valorViagem)
+console.log(valorViagem);
