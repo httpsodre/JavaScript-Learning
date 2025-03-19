42e3a8b8471917265d0e525106b40d77e0c91cba
httpsodre <camposalansodre@gmail.com>
2025-03-19 15:37:59 -0300

    teste1

---
+++ praticando variaveis, operadores e estruturas condicionais/desafio_03.js
@@ -0,0 +1,15 @@
+/* Media de Notas */
+
+const nota1 = 8;
+const nota2 = 7;
+const nota3 = 9;
+
+const media =(nota1 + nota2 + nota3)/3;
+
+if (media > 7) {
+    console.log("Aprovado!")
+}else if (media <= 7 ) {
+    console.log ("Recuperação!")
+} else (media < 5);{
+    console.log("Reprovado!")
+}
