cbe36e6d5d2693fdd250287397810149896436fa
httpsodre <camposalansodre@gmail.com>
2025-03-20 10:34:57 -0300

    teste1

---
+++ 04-função/desafio_07.js
@@ -0,0 +1,5 @@
+/* Desafio para verificar se uma pessoa é maior de idade ou não. */
+function verificarIdade(idade) {
+    if (idade < 18) { return 'menor de idade' } else {return 'maior de idade'}};
+
+    console.log(verificarIdade(17));
