29f48047e11b8ae2d05af457afb17ee4d2329766
httpsodre <camposalansodre@gmail.com>
2025-03-20 14:24:51 -0300

    teste1

---
+++ 05-Objetos e Classes/desafio_10.js
@@ -0,0 +1,25 @@
+/* classe para representar carros e gasto durante certo trajeto*/
+class Carro {
+    constructor(marca, cor, kmPorLitro) {
+        this.marca = marca;
+        this.cor = cor;
+        this.kmPorLitro = kmPorLitro;
+    }
+}
+class Viagem {
+    constructor(carro, distancia, precoLitro) {
+        this.carro = carro;
+        this.distancia = distancia;
+        this.precoLitro = precoLitro;
+    }
+    obterAutonomia() {
+        return this.carro.kmPorLitro * this.distancia;
+    }
+    calcularCusto() {
+        return this.distancia / this.carro.kmPorLitro * this.precoLitro;
+    }
+}
+const carro = new Carro('Chevrolet', 'Prata', 10);
+const viagem = new Viagem(carro, 100, 4.5);
+console.log(viagem.calcularCusto()); 
+
