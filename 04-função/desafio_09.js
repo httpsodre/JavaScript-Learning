e4b4b7129349b781de83ab5fda724681b563eee7
httpsodre <camposalansodre@gmail.com>
2025-03-20 11:05:24 -0300

    teste juros

---
+++ 04-função/desafio_09.js
@@ -0,0 +1,25 @@
+/* desafio de precificação*/
+
+
+
+
+
+function desconto (valor, desconto){
+    return (valor - (valor * (desconto/100)))
+}
+function juros (valor, juros){
+    return (valor + (valor *(juros/100)))
+}
+const valorProduto = 100;
+const formaDePagamento = 4;
+
+if (formaDePagamento === 1){
+console.log(desconto(valorProduto, 10))
+} else if (formaDePagamento === 2){
+    console.log(desconto(valorProduto,15))
+}else if (formaDePagamento === 3){
+    console.log(valorProduto)
+}else {
+    console.log(juros(valorProduto, 10))
+}
+
